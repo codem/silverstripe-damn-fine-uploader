13a632d64f20190a2c3f17a8ae135b2fe5300029
James Ellis <james.ellis@dpc.nsw.gov.au>
2018-06-22 06:37:23 +0000

    Webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,6 @@
+'use strict';
 const Path = require('path');
-const webpack = require('webpack');
-// Import the core config
-const webpackConfig = require('@silverstripe/webpack-config');
-const {
-  resolveJS,
-  externalJS,
-  moduleJS,
-  pluginJS,
-  moduleCSS,
-  pluginCSS,
-} = webpackConfig;
-
-const ENV = process.env.NODE_ENV;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const PATHS = {
   // the root path, where your webpack.config.js is located.
   ROOT: Path.resolve(),
@@ -26,122 +15,54 @@ const PATHS = {
   DIST: Path.resolve('client/dist')
 };
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const autoprefixer = require('autoprefixer');
-
-const config = [
-  {
-    name: 'js',
-    entry: {
-      traditionalcore: [
-        `${PATHS.MODULES}/fine-uploader/fine-uploader/fine-uploader.core.js`,
-        `${PATHS.SRC}/js/dfu.core.js`
-      ],
-      traditionalui: [
-        `${PATHS.MODULES}/fine-uploader/fine-uploader/fine-uploader.js`,
-        `${PATHS.SRC}/js/dfu.ui.js`
-      ]
-    },
-    output: {
-      path: PATHS.DIST,
-      filename: 'js/[name].js'
-    },
-    devtool: (ENV !== 'production') ? 'source-map' : '',
-    resolve: Object.assign({}, resolveJS(ENV, PATHS), {extensions: ['.json', '.js', '.jsx']}),
-    externals: externalJS(ENV, PATHS),
-    module: moduleJS(ENV, PATHS),
-    plugins: pluginJS(ENV, PATHS)
+module.exports = {
+  mode : 'development',
+  entry : PATHS.SRC + '/index.js',
+  output: {
+    path: PATHS.DIST,
+    filename: 'js/[name]'
   },
-  {
-    name: 'css',
-    entry: {
-      traditionalcore: [
-        `${PATHS.SRC}/styles/dfu.core.css`
-      ],
-      traditionalui: [
-        `${PATHS.MODULES}/fine-uploader/fine-uploader/fine-uploader-gallery.css`,
-        `${PATHS.SRC}/styles/dfu.ui.css`
-      ]
-    },
-    output: {
-      path: PATHS.DIST,
-      filename: 'styles/[name].css',
-    },
-    devtool: (ENV !== 'production') ? 'source-map' : '',
-    module: {
-      rules: [
-        {
-          test: /\.(scss|css)$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: "css-loader",
-              options: {
-                  url : false,
-                  minimize: {
-                      safe: true
-                  },
-                  sourceMap: true
-              }
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                autoprefixer: {
-                  browsers: [
-                    ">0.25%"
-                  ]
-                },
-                plugins: () => [
-                  autoprefixer
-                ],
-                sourceMap: true
-              },
-            },
-            'sass-loader',
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: "[name].css",
-        chunkFilename: "[id].css"
-      })
-    ]
-  },
-  {
-    name: 'assets',
-    entry: {
-      traditionalui: [
-        `${PATHS.MODULES}/fine-uploader/fine-uploader/loading.gif`,
-        `${PATHS.MODULES}/fine-uploader/fine-uploader/processing.gif`,
-        `${PATHS.MODULES}/fine-uploader/fine-uploader/continue.gif`,
-        `${PATHS.MODULES}/fine-uploader/fine-uploader/edit.gif`,
-        `${PATHS.MODULES}/fine-uploader/fine-uploader/pause.gif`,
-        `${PATHS.MODULES}/fine-uploader/fine-uploader/retry.gif`,
-        `${PATHS.MODULES}/fine-uploader/fine-uploader/trash.gif`,
-        `${PATHS.MODULES}/fine-uploader/all.fine-uploader/placeholders/not_available-generic.png`,
-        `${PATHS.MODULES}/fine-uploader/all.fine-uploader/placeholders/waiting-generic.png`
-      ]
-    },
-    output: {
-      path: `${PATHS.DIST}/assets/`,
-    },
-    module: {
-      rules : [
-        {
-          test: /\.(gif|png|jpg|webp)$/,
-          use: {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-            },
-          }
-        }
-      ]
-    }
-  }
-];
-
-module.exports = config;
+  plugins: [
+    new CopyWebpackPlugin([
+      // Core
+      {
+        'from' : PATHS.MODULES + '/fine-uploader/fine-uploader/fine-uploader.core.js',
+        'to' : PATHS.DIST + '/js/traditional.core.js',
+      },
+      {
+        'from' : PATHS.SRC + '/js/dfu.core.js',
+        'to' : PATHS.DIST + '/js/dfu.core.js',
+      },
+      {
+        'from' : PATHS.SRC + '/styles/dfu.core.css',
+        'to' : PATHS.DIST + '/styles/dfu.core.css',
+      },
+      // UI
+      {
+        'from' : PATHS.SRC + '/js/dfu.ui.js',
+        'to' : PATHS.DIST + '/js/dfu.ui.js',
+      },
+      {
+        'from' : PATHS.SRC + '/styles/dfu.ui.css',
+        'to' : PATHS.DIST + '/styles/dfu.ui.css',
+      },
+      {
+        'from' : PATHS.MODULES + '/fine-uploader/fine-uploader/fine-uploader.js',
+        'to' : PATHS.DIST + '/js/traditional.ui.js',
+      },
+      {
+        'from' : PATHS.MODULES + '/fine-uploader/fine-uploader/fine-uploader-gallery.css',
+        'to' : PATHS.DIST + '/styles/traditional.ui.gallery.css',
+      },
+      // ASSETS (UI)
+      {
+        'from' : PATHS.MODULES + '/fine-uploader/fine-uploader/*.gif',
+        'to' : PATHS.DIST + '/assets/[name].[ext]',
+      },
+      {
+        'from' : PATHS.MODULES + '/fine-uploader/all.fine-uploader/placeholders/*.png',
+        'to' : PATHS.DIST + '/assets/placeholders/[name].[ext]',
+      }
+    ])
+  ]
+};
