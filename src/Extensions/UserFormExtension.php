c9fa0ee25a7a4573677daada866f29d702ff014d
James Ellis <james@host.dpcdev>
2019-03-20 06:13:36 +0000

    UDF: field can be any instance of DamnFineUploaderField, update doco with reference to set form action firing order

--- src/Extensions/UserFormExtension.php
+++ src/Extensions/UserFormExtension.php
@@ -3,17 +3,21 @@ namespace Codem\DamnFineUploader;
 use SilverStripe\Core\Extension;
 use SilverStripe\Control\Controller;
 
+/**
+ * Handle field modifications in a UserForm context
+ * In a userform context, the controller request endpoint is 'Form' rather than the Form ID/Name
+ * $form->setFormAction is called after this is run, so 'Form' is required here to correctly reference the form
+ */
 class UserFormExtension extends Extension {
 
   public function updateForm($fields) {
     $fields = $this->owner->Fields()->dataFields();
     foreach($fields as $field) {
-      if($field instanceof FineUploaderCoreField || $field instanceof FineUploaderField) {
+      if($field instanceof DamnFineUploaderField) {
         // ensure the field has the current form's SecurityToken
         $field->setSecurityToken( $this->owner->getSecurityToken() );
         // and the request endpoint, now that a Form is available on the field
         $link = Controller::join_links( $this->owner->getController()->Link('Form'), $field->UploadLink() );
-        //https:\/\/www.nsw.gov.au\/your-government\/ministers\/minister-for-resources-minister-for-energy-and-utilities-minister-for-the-arts-and-vice-president-of-the-executive-council\/Form\/field\/EditableTextField_4e49d\/upload
         $field->setRequestEndpoint($link);
       }
     }
