0e82d2984359fe4d5d38e401a82a4c7fad30dda2
James Ellis <james@host.dpcdev>
2019-03-21 03:46:09 +0000

    Add image dimension validation to Uppy implementation

--- client/src/js/uppy.js
+++ client/src/js/uppy.js
@@ -18,20 +18,24 @@ if(dfu_uploaders_uppy) {
     var dfu = new DFU();
     dfu.init();
 
-    var uploader;
-    var config = JSON.parse(upload_element.dataset.config);
-    var id = upload_element.id;
+    const config = JSON.parse(upload_element.dataset.config);
+    const id = upload_element.id;
     var meta = {};
     if(typeof config.request.params == 'object') {
       meta = config.request.params;
     }
 
-    var max_file_size = config.validation.sizeLimit ? config.validation.sizeLimit : null;
-    var max_num_files = config.validation.itemLimit ? config.validation.itemLimit : null;
-    var min_num_files = null;
-    var allowed_file_types = config.validation.acceptFiles ? config.validation.acceptFiles.split(',') : ['image/*' ];// default to images
+    const max_file_size = config.validation.sizeLimit ? config.validation.sizeLimit : null;
+    const max_num_files = config.validation.itemLimit ? config.validation.itemLimit : null;
+    const min_num_files = null;
+    const allowed_file_types = config.validation.acceptFiles ? config.validation.acceptFiles.split(',') : ['image/*' ];// default to images
 
-    var restrictions = {
+    const max_image_width = config.validation.image.maxWidth ? config.validation.image.maxWidth : 0;
+    const max_image_height = config.validation.image.maxHeight ? config.validation.image.maxHeight : 0;
+    const min_image_width = config.validation.image.minWidth ? config.validation.image.minWidth : 0;
+    const min_image_height = config.validation.image.minHeight ? config.validation.image.minHeight : 0;
+
+    const restrictions = {
       maxFileSize: max_file_size,
       maxNumberOfFiles: max_num_files,
       minNumberOfFiles: min_num_files,
@@ -52,7 +56,8 @@ if(dfu_uploaders_uppy) {
                       target: upload_element.querySelector('.dashboard'),
                       inline: true,
                       showLinkToFileUploadResult: false,
-                      proudlyDisplayPoweredByUppy: false
+                      proudlyDisplayPoweredByUppy: false,
+                      note: '' // TODO
                     }
                   )
                   .use(
@@ -87,9 +92,54 @@ if(dfu_uploaders_uppy) {
     });
 
     uppy.on('file-added', (file) => {
+
+      // set required file meta
       var meta = {};
       meta[ config.request.uuidName ] = file.id;
       uppy.setFileMeta( file.id, meta );
+
+      if( dfu.isImage(file.data.type)) {
+
+        // handle image dimension validation
+        const data = file.data;
+        const url = URL.createObjectURL(data);
+        const image = new Image();
+        image.src = url;
+        image.onload = () => {
+
+          var remove = false;
+          var message = '';
+
+          if(max_image_width > 0 && image.width > max_image_width) {
+            message = config.messages.maxWidthImageError;
+            remove = true;
+          } else if(max_image_height > 0 && image.height > max_image_height) {
+            message = config.messages.maxHeightImageError;
+            remove = true;
+          } else if(min_image_width > 0 && image.width < min_image_width) {
+            message = config.messages.minWidthImageError;
+            remove = true;
+          } else if(min_image_height > 0 && image.height < min_image_height) {
+            message = config.messages.minHeightImageError;
+            remove = true;
+          }
+
+          if(remove) {
+            if(!message) {
+              message = 'The image does not match the allowed dimensions';
+            }
+            uppy.removeFile(file.id);
+            uppy.info({
+              message: message,
+              type: 'error',
+              duration: 7500
+            });
+            URL.revokeObjectURL(url);
+          }
+
+        };
+      }
+
     });
 
   };
